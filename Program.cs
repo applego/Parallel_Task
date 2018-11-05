3ea2d9d9ba27f92b166fba175aba21f8d9258f46
Tsuneshige Yusuke <tokoshige_mac@TsuneshigenoMacBook-Pro.local>
2018-11-05 23:18:53 +0900

    add task

--- Program.cs
+++ Program.cs
@@ -22,7 +22,17 @@ namespace ParallelProgrammingwithCsharp
             const int numThreads = 10;
             var primes_5_Parallel = new List<int>[numThreads];
             Parallel.For(0,numThreads,i => primes_5_Parallel[i] = GetPrimeNumbers(i==0 ? 2:i*1000000 + 1, (i+1)*1000000));
-            Console.WriteLine($"_5_Parallel Primes founc: {primes_5_Parallel.Sum(p => p.Count)}, Total time: {sw_5_Parallel.ElapsedMilliseconds}");
+            Console.WriteLine($"_5_Parallel Primes found: {primes_5_Parallel.Sum(p => p.Count)}, Total time: {sw_5_Parallel.ElapsedMilliseconds}");
+            //_6_Task
+            var sw_6_Tasks = new Stopwatch();
+            sw_6_Tasks.Start();
+            var primes_6_Tasks = new Task<List<int>>[numThreads];
+            for(int i = 0; i<numThreads; i++){
+                int index = i;
+                primes_6_Tasks[i] = Task.Factory.StartNew(() => GetPrimeNumbers(index == 0 ? 2 : index*1000000+1, (index+1)* 1000000));
+            }
+            Task.WaitAll(primes_6_Tasks);
+            Console.WriteLine($"_6_Tasks Primes found: {primes_6_Tasks.Sum(p => p.Result.Count)}, Total time:{sw_6_Tasks.ElapsedMilliseconds}");
         }
 
         private static List<int> GetPrimeNumbers(int minimum, int maximum){
