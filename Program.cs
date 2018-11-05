9eae9b0780b838dcc7dbc0bc9ec3d363ee52b433
Tsuneshige Yusuke <tokoshige_mac@TsuneshigenoMacBook-Pro.local>
2018-11-06 01:31:53 +0900

    add タスクを極めろ(Qiita)

--- Program.cs
+++ Program.cs
@@ -40,10 +40,25 @@ namespace ParallelProgrammingwithCsharp
             Console.WriteLine($"_7_PLINQ Primes found:{primes_7_PLINQ.Count},Total time:{sw_7_PLINQ.ElapsedMilliseconds}");
             //_8_Async
             ProcessPrimesAsync();
-            Console.ReadLine();
+            //Console.ReadLine();
             //_9_ParallelAsync
             ProcessPrimesAsync_ParallelAsync();
             Console.ReadLine();
+
+            //Taskを極めろ！
+            var task = Task.Delay(1000); //ただのDelayなんだけど、ちょっと違って見えてきません?
+            var client = new System.Net.Http.HttpClient();
+            var task2 = client.GetAsync("https://www.amazon.co.jp/");//awaitをつけていない
+            /*
+            「タスク」であるという意味
+非同期メソッドはタスクであり、作業手順書です。手順書であるということは、
+それが「書かれた通りの順序で実行していけばいい」ということであり、
+言い換えるとそれは「記述されたタスクの実行順序さえ間違えなければ、
+誰が（どのスレッドが）どのタスクを実行したって構わない」と表明しているような事になります。
+つまり、Taskとは「スレッドの存在を意識する必要がない、単なる『処理』のまとまり」だということです。
+非同期処理を「タスク」の組み合わせとみなすことで、処理を分割しやすく、そして容易に合成できるようにしたもの。
+それが、Taskクラスなのです。
+            */
         }
 
         private static List<int> GetPrimeNumbers(int minimum, int maximum){
@@ -89,5 +104,24 @@ namespace ParallelProgrammingwithCsharp
             var results = await Task.WhenAll(primes);
             Console.WriteLine($"_9_ParallelAsync Primes found:{results.Sum(p => p.Count)},Total time:{sw.ElapsedMilliseconds}");
         }
+        //タスクを極めろ
+        async Task AsyncMethod(){
+            await Task.Delay(1000);//1000ミリ秒待機するという仕事の完了を待ち、
+            Console.WriteLine("Done!(IN AsyncMethod)");//Done!をコンソールに出力する
+        }//という、「一つのTask」を表す。
+        async Task<string> AsyncMethod2(Uri uri){
+            using(var client = new System.Net.Http.HttpClient())//<- 本当はHttpClientをusingで使っちゃだめ
+            {
+                var response = await client.GetAsync(uri);//<- Getせよのタスクを開始し、その完了を待機する
+                var text = await response.Content.ReadAsStringAsync();//レスポンスからその本文をstringとして読み出すタスクを開始し、その完了を待機する
+                return text;//読み出したtextを返す
+                /*
+                //↑ちゃんと順番通り実行してくれるなら、
+                var resonse = await client.GetAsync(uri);//このタスクを実行する人（スレッド）と
+                var text = await response.content.ReadasstringAsync();//このタスクを実行する人が違ったとしても
+                return text;//全く問題はない！
+                 */
+            }
+        }//という一つのタスク（Task<string>）を表す。
     }
 }
